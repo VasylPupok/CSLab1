40d5f88d917270021cff5513da5089a9e43fb568
VasylPupok <dejnekaartem223@gmail.com>
2023-02-11 21:41:57 +0200

    Add project files.

---
+++ Birthday.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Birthday
+    {
+        public Birthday(DateTime birthday) {
+            this._birthday = birthday;
+        }
+
+        public TimeSpan? Age {
+            get
+            {
+                TimeSpan? age = DateTime.Now - _birthday;
+                return (age > TimeSpan.Zero) ? age : null;
+            }
+        }
+
+        public string Zodiac
+        {
+            get
+            {
+                switch (this._birthday.Month)
+                {
+                    case 1:
+                        if (this._birthday.Day < 20)
+                        {
+                           return "Capricorn";
+                        }
+                        else
+                        {
+                            return "Aquarius";
+                        }
+                    case 2:
+                        if (this._birthday.Day < 19)
+                        {
+                            return "Aquarius";
+                        }
+                        else
+                        {
+                            return "Pisces";
+                        }
+                    case 3:
+                        if (this._birthday.Day < 21)
+                        {
+                            return "Pisces";
+                        }
+                        else
+                        {
+                            return "Aries";
+                        }
+                    case 4:
+                        if (this._birthday.Day < 20)
+                        {
+                            return "Aries";
+                        }
+                        else
+                        {
+                            return "Taurus";
+                        }
+                    case 5:
+                        if (this._birthday.Day < 21)
+                        {
+                            return "Taurus";
+                        }
+                        else
+                        {
+                            return "Gemini";
+                        }
+                    case 6:
+                        if (this._birthday.Day < 22)
+                        {
+                            return "Gemini";
+                        }
+                        else
+                        {
+                            return "Cancer";
+                        }
+                    case 7:
+                        if (this._birthday.Day < 23)
+                        {
+                            return "Cancer";
+                        }
+                        else
+                        {
+                            return "Leo";
+                        }
+                    case 8:
+                        if (this._birthday.Day < 24)
+                        {
+                            return "Leo";
+                        }
+                        else
+                        {
+                            return "Virgo";
+                        }
+                    case 9:
+                        if (this._birthday.Day < 24)
+                        {
+                            return "Virgo";
+                        }
+                        else
+                        {
+                            return "Libra";
+                        }
+                    case 10:
+                        if (this._birthday.Day < 23)
+                        {
+                            return "Libra";
+                        }
+                        else
+                        {
+                            return "Scorpio";
+                        }
+                    case 11:
+                        if (this._birthday.Day < 23)
+                        {
+                            return "Scorpio";
+                        }
+                        else
+                        {
+                            return "Sagittarius";
+                        }
+                    case 12:
+                        if (this._birthday.Day < 22)
+                        {
+                            return "Sagittarius";
+                        }
+                        else
+                        {
+                            return "Capricorn";
+                        }
+                    default:
+                        return "Error";
+                }
+            }
+        }
+
+        public string ChineseZodiac { 
+            get
+            {
+                return CHINESE_SIGNS[this._birthday.Year % 12];
+            } 
+        }
+
+        public bool BirthdayIsToday
+        {
+            get
+            {
+                return this._birthday.Day == DateTime.Now.Date.Day;
+            }
+        }
+
+        private static string[] CHINESE_SIGNS = new string[]
+        {
+            "Monkey",
+            "Rooster",
+            "Dog",
+            "Pig",
+            "Rat",
+            "Ox",
+            "Tiger",
+            "Rabbit",
+            "Dragon",
+            "Snake",
+            "Horse",
+            "Goat"
+        };
+
+        private readonly DateTime _birthday;
+
+    }
+
+    
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿
+using System;
+using System.Windows;
+
+
+namespace Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public const double DAYS_IN_YEAR = 365.25;
+        public const double MAX_ACCEPTABLE_AGE = 135;
+        public const double MAX_DAYS = DAYS_IN_YEAR * MAX_ACCEPTABLE_AGE;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Error("Birthday is not initialized");
+        }
+
+        public void birthdaySet(object sender, EventArgs e)
+        {
+            this._birthdayInfo = new Birthday(this._dateInput.SelectedDate.Value);
+            if (ValidBirthday())
+            {
+                CheckBirthday();
+                this._ageBlock.Text = $"Age: {((int)(this._birthdayInfo.Age.Value.Days / DAYS_IN_YEAR))}";
+                this._zodiacBlock.Text = $"Zodiac sign: {this._birthdayInfo.Zodiac}";
+                this._zodiacBlockChinese.Text = $"Chinese zodiac sign: {this._birthdayInfo.ChineseZodiac}";
+            }
+
+        }
+
+
+        private Birthday _birthdayInfo = null;
+
+        private void CheckBirthday()
+        {
+            if (this._birthdayInfo.BirthdayIsToday)
+            {
+                this._happyBirthdayText.Text = "Happy birthday, human";
+            }
+            else
+            {
+                this._happyBirthdayText.Text = "";
+            }
+        }
+
+        private bool ValidBirthday()
+        {
+            if (_birthdayInfo == null)
+            {
+                Error("Birthday is not initialized");
+                return false;
+            }
+            else if (_birthdayInfo.Age == null)
+            {
+                Error("Program does not work with users, who haven't been born");
+                return false;
+            }
+            else if (_birthdayInfo.Age.Value.Days > MAX_DAYS)
+            {
+                Error(
+                    $"Humans I work with don't live longer than {MAX_ACCEPTABLE_AGE} years.\n " +
+                    $"Terminators are sent in the past to correct this mistake. Please, stand by"
+                    );
+                return false;
+            }
+            Success();
+            return true;
+        }
+
+        private void Success()
+        {
+            this._warningTextBlock.Text = "";
+        }
+
+        private void Error(string message)
+        {
+            this._ageBlock.Text = "Age: ";
+            this._zodiacBlockChinese.Text = "Zodiac: ";
+            this._zodiacBlock.Text = "Chinese zodiac: ";
+
+            this._warningTextBlock.Text = message;
+        } 
+
+    }
+}
